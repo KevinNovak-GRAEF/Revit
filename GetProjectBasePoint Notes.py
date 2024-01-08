31743928fe59a421940a2749880fef390f31f146
Kevin Novak <105993516+KevinNovak-GRAEF@users.noreply.github.com>
2024-01-08 12:22:23 -0600

    Create GetProjectBasePoint Notes.py
    Notes from the GetProjectBasePoint(doc) and .Position

---
+++ GetProjectBasePoint Notes.py
@@ -0,0 +1,6 @@
+base_point = BasePoint.GetProjectBasePoint(doc)
+base_point_position = base_point.Position
+base_point_position_z = base_point_position.Z
+survey_point = BasePoint.GetSurveyPoint(doc)
+survey_point_position = survey_point.Position
+survey_point_position_z = survey_point_position.Z
