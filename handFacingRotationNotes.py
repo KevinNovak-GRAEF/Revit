d1bad1410b8c4e6224c750635779785e4d9de216
Kevin Novak <105993516+KevinNovak-GRAEF@users.noreply.github.com>
2024-02-18 15:43:21 -0600

    Update handFacingRotationNotes.py

--- handFacingRotationNotes.py
+++ handFacingRotationNotes.py
@@ -8,3 +8,14 @@ facingZ = facing.Z
 slope = math.degrees(math.asin(handZ))
 facingSlopeZ = facingZ / (math.cos(math.radians(slope)))
 crossSectionRotation = math.degrees(math.asin(facingSlopeZ))
+
+crossRotationPara = BuiltInParameter.STRUCTURAL_BEND_DIR_ANGLE
+
+elementRotationRad = element.get_Parameter(crossRotationPara).AsDouble()
+elementRotationDeg = math.degrees(elementRotationRad)
+
+if elementRotationRad <= math.pi:
+    sdsRotationRad = elementRotationRad * -1
+    sdsRotationDeg = math.degrees(sdsRotationRad)
+else:
+    sdsRotationDeg = 360 - elementRotationDeg
