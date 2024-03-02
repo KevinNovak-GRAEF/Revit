23ab9e710d14555e0a4a42f41ad6ce34f1a2e29e
Kevin Novak <105993516+KevinNovak-GRAEF@users.noreply.github.com>
2024-03-02 14:30:17 -0600

    Update filterByView.py

--- filterByView.py
+++ filterByView.py
@@ -9,3 +9,13 @@ viewFilter = ElementParameterFilter(viewRule)
 viewIFC = FilteredElementCollector(doc).OfClass(View3D).WherePasses(viewFilter).FirstElementId()
 
 columns = FilteredElementCollector(doc, viewIFC).OfCategory(ColumnCategory).WhereElementIsNotElementType().ToElements()
+
+# PickObject(s) use list for objects
+reference = uidoc.Selection.PickObjects(Selection.ObjectType.Element)
+elmntList = []
+for ref in reference:
+    elmnt = doc.GetElement(ref)
+    elmntList.append(elmnt)
+
+# PickElementsByRectangle
+reference = uidoc.Selection.PickElementsByRectangle()
