91c344fa5b009cba44d451d581d1e285dbe1fa5b
Kevin Novak <105993516+KevinNovak-GRAEF@users.noreply.github.com>
2024-03-02 19:08:08 -0600

    Update GetProjectBasePoint Notes.py

--- GetProjectBasePoint Notes.py
+++ GetProjectBasePoint Notes.py
@@ -4,3 +4,31 @@ base_point_position_z = base_point_position.Z
 survey_point = BasePoint.GetSurveyPoint(doc)
 survey_point_position = survey_point.Position
 survey_point_position_z = survey_point_position.Z
+
+baseElevationParam = BuiltInParameter.BASEPOINT_ELEVATION_PARAM
+baseNSParam = BuiltInParameter.BASEPOINT_NORTHSOUTH_PARAM
+baseEWParam = BuiltInParameter.BASEPOINT_EASTWEST_PARAM
+
+reference = uidoc.Selection.PickObject(Selection.ObjectType.Element)
+elmnt = doc.GetElement(reference)
+
+projectBase = BasePoint.GetProjectBasePoint(doc)
+projectBasePosition = projectBase.Position
+projectBasePositionX = projectBasePosition.X
+projectBasePositionY = projectBasePosition.Y
+projectBasePositionZ = projectBasePosition.Z
+
+curve = elmnt.Location.Curve
+startPoint = curve.GetEndPoint(0)
+startPointX = startPoint.X
+startPointY = startPoint.Y
+startPointZ = startPoint.Z
+
+
+
+
+startPointXPB = startPointX - projectBasePositionX
+startPointYPB = startPointY - projectBasePositionY
+startPointZPB = startPointZ - projectBasePositionZ
+
+startPointPB = XYZ(startPointXPB, startPointYPB, startPointZPB)
